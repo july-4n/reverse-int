ee06fc660c91f1f84b65d563e3e4f0e6cacacf5a
July-4n <september4n@gmail.com>
2022-01-08 19:10:47 +0500

    feat: solved the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let positiveNumber = Math.abs(n);
+
+    let reverseString = String(positiveNumber).split("").reverse().join("");
+
+    return Number(reverseString);
 }
